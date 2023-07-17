438b738cce51334f445981d9f2ca8b35909d0dc9
sxnz z <sxnz@gmail.com>
2023-07-17 15:40:11 +0100

    [WE-1] Finished

--- test.rb
+++ test.rb
@@ -1,14 +1,17 @@
 def greater_than_5 arr
   count = 0
+found_numbers = []
 
-  arr.count do |e| 
-    e > 5
-  end
+  arr.count do |e|
+    if ( e > 5) 
+	count = count + 1
+	found_numbers.push(e)
+	end
+  end  
 
-  #print counted value 
+  #print counted value
+  puts "Total count: #{count}, numbers found are #{found_numbers}"
 end
-
- 
 
 arr = [17, 7, 3, 6, 10, 1]
 greater_than_5 arr
